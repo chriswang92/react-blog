13cb600476bc8cacd0dde4db98ea79996205eabd
Chris Wang <chriswang@Chriss-MacBook-Pro.local>
2019-10-07 22:06:15 -0400

    added blogtable and blogcard component base

--- src/login-form/components/BasicLayout.js
+++ src/login-form/components/BasicLayout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Button} from 'antd';
+import BlogTable from './BlogTable';
+
 class BasicLayout extends React.Component {
   render() {
 
     return (
       <div>
-        <h1>BasicLayout</h1>
-        
+        <h1>BasicLayout123</h1>
+        <BlogTable />
         <Button onClick={()=>this.props.history.push('/login')}>login </Button>
       </div>
     );
